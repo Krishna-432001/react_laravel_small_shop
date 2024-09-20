ced6ade4120da7bf4ed8477ecde9d582b7dc0dc1
krishna-432001 <krishnapriya432001@gmail.com>
2024-09-20 12:00:20 +0530

    footer can added logic

--- src/components/frontend/layout/Footer.jsx
+++ src/components/frontend/layout/Footer.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer>
-      {/* Footer content */}
-      Footer Section
-    </footer>
-  );
-};
+    return (
+        <footer className="bg-dark text-white py-4">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <h5>About Us</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla sit amet volutpat.</p>
+                    </div>
+                    <div className="col-md-4">
+                        <h5>Links</h5>
+                        <ul className="list-unstyled">
+                            <li><a href="#" className="text-white">Home</a></li>
+                            <li><a href="#" className="text-white">Features</a></li>
+                            <li><a href="#" className="text-white">Pricing</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4">
+                        <h5>Contact</h5>
+                        <p>Email: example@example.com</p>
+                        <p>Phone: (123) 456-7890</p>
+                    </div>
+                </div>
+            </div>
+            <div className="text-center py-3">
+                &copy; 2024 Your Company
+            </div>
+        </footer>
+    );
+}
 
 export default Footer;
