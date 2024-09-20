2795fd1c8c6267b9c3a7cce4811ada66046c4013
krishna-432001 <krishnapriya432001@gmail.com>
2024-09-20 12:14:20 +0530

    routes are added home profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/frontend/HomePage'
 import Login from './components/frontend/auth/Login'
 import Register from './components/frontend/auth/Register'
 import ForgetPassword from './components/frontend/auth/ForgetPassword'
+import Profile from './components/frontend/auth/Profile'
 import AppLayout from './components/frontend/layout/AppLayout';
 
 const App = () => {
@@ -13,8 +15,10 @@ const App = () => {
       <AppLayout>
 
         <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
           <Route path="/forget-password" element={<ForgetPassword />} />
           {/* Add more routes here */}
         </Routes>
